62374ad66ccf02eb3efafa108094173077da62db
carlos hernandez <carloshernandez@MacBook-Air-de-carlos.local>
2025-02-20 23:28:08 -0400

    Add Login and Logout, and authmiddleware

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,21 +1,26 @@
 const Usuario = require('../models/Usuario');
+const logger = require('../utils/logger');
 
 class AuthController {
     async iniciarSesion(req, res) {
         try {
             const { email, contrasena } = req.body;
             const { token, usuario } = await Usuario.iniciarSesion(email, contrasena);
+            logger.info(`Usuario ${usuario.nombre} ha iniciado sesión correctamente`);
             res.json({ token, usuario });
         } catch (error) {
-            res.status(401).json({ message: error.message }); // 401 Unauthorized
+            logger.error(`Error al iniciar sesión: ${error.message}`);
+            res.status(401).json({ message: error.message });
         }
     }
 
     async cerrarSesion(req, res) {
         try {
+            logger.info(`Usuario ${req.usuario.nombre} ha cerrado sesión correctamente`);
             res.json({ mensaje: 'Sesión cerrada correctamente' });
         } catch (error) {
-            res.status(401).json({ message: error.message }); // 401 Unauthorized
+            logger.error(`Error al cerrar sesión: ${error.message}`);
+            res.status(401).json({ message: error.message });
         }
     }
 
@@ -23,8 +28,11 @@ class AuthController {
         try {
             const { refreshToken } = req.body;
             const { token } = await Usuario.renovarToken(refreshToken);
+
+            logger.info(`Usuario ${req.usuario.nombre} ha renovado su token correctamente`);
             res.json({ token });
         } catch (error) {
+            logger.error(`Error al renovar token: ${error.message}`);
             res.status(401).json({ message: error.message });
         }
     }
